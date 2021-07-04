262a90289d1b7fe7ce72b6fb858558fb79bd9e57
RhodryCann <85023762+RhodryCann@users.noreply.github.com>
2021-07-04 12:48:31 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 
-const getStocks = () => (
-    fetch("https://twelve-data1.p.rapidapi.com/stocks?exchange=LSE&format=json", {
+const getAllStocks = () => (
+    fetch("https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json", {
 		"method": "GET",
 		"headers": {
-			"x-rapidapi-key": "1ffa19187dmsha38e8760b594827p10c25djsn9cdeddb128a5",
+			"x-rapidapi-key": "5833e0152dmsh4d85cf98bb55712p149824jsnc0cb33c54d5d", //hide this key
+			"x-rapidapi-host": "twelve-data1.p.rapidapi.com"
+		}
+	})
+);
+
+const getStockQuote = (symbol) => (
+	fetch(`https://twelve-data1.p.rapidapi.com/quote?symbol=${symbol}&interval=1day&outputsize=30&format=json`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "5833e0152dmsh4d85cf98bb55712p149824jsnc0cb33c54d5d", //hide this key
 			"x-rapidapi-host": "twelve-data1.p.rapidapi.com"
 		}
 	})
@@ -12,19 +22,32 @@ const getStocks = () => (
 
 const Home = (props) => {
 	const [stocks, setStocks] = useState([]);
-	getStocks()
-		.then(response => response.json()
-		.then(({ data }) => setStocks(data)));
+	const [price, setPrice] = useState("");
+	// to do: in stocks.map call new component, <Stock />
     return (
 		<div>
-        	<button onClick={() => alert('clicked')}>Refresh</button>
-			<ul>
+        	<button onClick={() => 
+				getAllStocks()
+					.then(response => response.json()
+					.then(({ data }) => setStocks(data)))
+			}>Refresh</button>
 				{
 					stocks.map(stock => 
-						<li>{stock.currency}</li>
+						<ul> 
+							<li>{stock.symbol}</li>
+							<li>{stock.name}</li>
+							<li>{stock.currency}</li>
+							<li>{stock.exchange}</li>
+							<button onClick={() => {
+								const symbol = stock.symbol;
+								getStockQuote(symbol)
+									.then(response => response.json()
+									.then((json) => setPrice(json)))
+							}}>price</button>
+							<li>PriceHigh: {price.high}</li>
+						</ul>
 					)
 				}
-			</ul>
 		</div>
 	);
 };
