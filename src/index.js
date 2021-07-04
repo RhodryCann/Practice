3c2aa27fe1f0d652071a5534c7d3ee3298363d9a
mg-charlotteneill <84379331+mg-charlotteneill@users.noreply.github.com>
2021-07-04 10:11:09 +0100

    Removed app function - not needed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./pages/Home";
 
-function App() {
-    return (
-      <div>
-        <Home />
-      </div>
-    );
-  }
-
 ReactDOM.render(
-    <App />,
+    <Home />,
     document.getElementById('app')
 );
-//<button onClick={() => { console.log('clicked'); refreshData()}}>Refresh</button>,
