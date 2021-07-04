181bb30b16f780ad6d570d365c1b00905ce1265d
RhodryCann <85023762+RhodryCann@users.noreply.github.com>
2021-07-04 11:42:05 +0100

    Displayed stock data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const getStocks = () => (
     fetch("https://twelve-data1.p.rapidapi.com/stocks?exchange=LSE&format=json", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1ffa19187dmsha38e8760b594827p10c25djsn9cdeddb128a5",
-		"x-rapidapi-host": "twelve-data1.p.rapidapi.com"
-	}
-})
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "1ffa19187dmsha38e8760b594827p10c25djsn9cdeddb128a5",
+			"x-rapidapi-host": "twelve-data1.p.rapidapi.com"
+		}
+	})
 );
 
 const Home = (props) => {
+	const [stocks, setStocks] = useState([]);
+	getStocks()
+		.then(response => response.json()
+		.then(({ data }) => setStocks(data)));
     return (
-        <button onClick={() => { console.log('clicked'); getStocks()}}>Refresh</button>
-    );
+		<div>
+        	<button onClick={() => alert('clicked')}>Refresh</button>
+			<ul>
+				{
+					stocks.map(stock => 
+						<li>{stock.currency}</li>
+					)
+				}
+			</ul>
+		</div>
+	);
 };
 
 export default Home;
